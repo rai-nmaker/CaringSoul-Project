d344df904b80d9e785d5e0946f3a1e815ca563a7
Jason Madrid <Jbammadrid00@gmail.com>
2022-03-05 00:30:02 +0800

    Boss wave 1

---
+++ Caring Soul/Assets/Boss.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public Animator animator;
+
+    public int MaxHealth = 300;
+    int CurrentHealth;
+
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
+         
+        if(CurrentHealth <= 0);
+        {
+            Die();
+        }
+    }
+     
+    void Die()
+    {
+        Debug.Log("Enemy Died!");
+
+        animator.SetBoolean("IsDead", true);
+
+        GetComponen<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
+
